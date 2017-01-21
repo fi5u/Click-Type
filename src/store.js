2b8a1bb683b6ed8baf98b3ac338f66cfba977123
Tommy Fisher <tommybfisher@gmail.com>
2017-01-21 21:21:06 +0200

    Do not log tick action

--- src/store.js
+++ src/store.js
@@ -3,7 +3,9 @@ import { createStore, applyMiddleware } from 'redux'
 import createLogger from 'redux-logger'
 import rootReducer from './reducers'
 
-const loggerMiddleware = createLogger()
+const loggerMiddleware = createLogger({
+    predicate: (getState, action) => action.type !== 'TICK '
+})
 
 export default function configureStore(preloadedState) {
     return createStore(
